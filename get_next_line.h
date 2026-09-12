a88b2a1fb060351e7b6b4ce55628ef6541fd0ac6
rimayer <reyam6252@gmail.com>
2026-09-12 22:52:37 +0200

    header skeleton

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,20 @@
 /*   By: reyam <reyam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/12 22:23:56 by reyam             #+#    #+#             */
-/*   Updated: 2026/09/12 22:23:58 by reyam            ###   ########.fr       */
+/*   Updated: 2026/09/12 22:51:46 by reyam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// read, malloc, free
+
+#ifndef get_next_line.h
+# define  get_next_line.h
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+
+
+#endif
